8ee9a106d1d5f2c89050d938231c321b7d463d88
xiaotian86www <xiaotian86www@163.com>
2022-11-25 12:05:12 +0800

    新增ci流程

--- src/test_hashmap.cpp
+++ src/test_hashmap.cpp
@@ -1,16 +1,10 @@
 #include "hashmap.hpp"
 #include "gtest/gtest.h"
 
-TEST(suite, get_set)
-{
-    dc::lock_free::HashMap<int, int> map;
-    map[1] = 1;
-    EXPECT_EQ(map[1], 2);
-}
-
-TEST(suite, get_set_2)
+TEST(hashmap, get_set)
 {
     dc::lock_free::HashMap<int, int> map;
     map[1] = 1;
     EXPECT_EQ(map[1], 1);
 }
+
